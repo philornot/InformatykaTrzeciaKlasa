ad1cfb22a33669f0d811ecf595349f9244044655
filip.pocztarski <filip.pocztarski@gmail.com>
2024-09-18 09:11:14 +0200

    niedziałający interfejs

---
+++ matura2024maj/Zadanie2/21.py
@@ -0,0 +1,5 @@
+from interfejs import algorytm
+
+n = int(input("Wpisz n: "))
+c, liczba_c_plus_b = algorytm(n)
+print(f'c={c}\nliczba wyk. instr: {liczba_c_plus_b}')
--- matura2024maj/Zadanie2/interfejs.py
+++ matura2024maj/Zadanie2/interfejs.py
@@ -1,30 +1,14 @@
-def algorytm(lista_liczb_n, b=1, c=0):
-    try:
-        for n in lista_liczb_n:
-            n_origin = n
-            a = n % 10
-            n = n % 10
-            if a % 2 == 0:
-                c = c + b * (a % 2)
-                print(f'dla {n_origin} c={c}')
-            else:
-                c = c + b
-                print(f'dla {n_origin} c={c}')
-            b = b * 10
-
-    except TypeError:
-        n = lista_liczb_n
-        n_origin = n
+def algorytm(n):
+    b = 1
+    c = 0
+    licznik_c_plus_b = 0
+    while n > 0:
         a = n % 10
-        n = n % 10
+        n = n // 10
         if a % 2 == 0:
             c = c + b * (a % 2)
-            print(f'dla {n_origin} c={c}')
         else:
             c = c + b
-            print(f'dla {n_origin} c={c}')
+            licznik_c_plus_b += 1
         b = b * 10
-
-
-# noinspection PyTypeChecker
-algorytm(10)
+    return c, licznik_c_plus_b
