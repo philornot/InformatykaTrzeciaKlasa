65cb0194ed81f389982048c5ed3678fe117e2c0b
franciszek.alaba <owcarasowa@gmail.com>
2024-09-23 09:18:54 +0200

    zadanie 4 początek

--- matura2024maj/Zadanie4/Franek/4-1.py
+++ matura2024maj/Zadanie4/Franek/4-1.py
@@ -1,21 +1,5 @@
-L = list(open("liczby.txt", "r"))
-L[0] = L[0] + " "
-L[1] = L[1] + " "
-L1 = []
-L2 = []
-a = ""
-for i in range(len(L[0])):
-    if L[0][i] == " ":
-        L1.append(int(a.strip()))
-        a = ""
-    else:
-        a = a + L[0][i]
-
-b=''
-for i in range(len(L[1])):
-    if L[1][i] == " ":
-        L2.append(int(b))
-        b = ""
-    else:
-        b = b + L[1][i]
-
+from wczy4 import L1,L2
+'''
+print(L1)
+print(L2)
+'''
