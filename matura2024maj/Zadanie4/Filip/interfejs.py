a1e1d31f83e9ccda3280d5001696bbceae3b67e1
filip.pocztarski <filip.pocztarski@gmail.com>
2024-09-30 09:29:02 +0200

    progres w 41

--- matura2024maj/Zadanie4/Filip/interfejs.py
+++ matura2024maj/Zadanie4/Filip/interfejs.py
@@ -1,2 +1,13 @@
-def load_line(line_number, file='../../matura2024ma/dane2024maj'):
-    with open(file)
+def load_line(line_number, file='../../../matura2024maj/dane2024maj/liczby_przyklad.txt'):
+    with open(file, encoding="utf-8") as f:
+        lines = f.readlines()
+    return lines[line_number - 1]
+
+
+def line_to_list(line_number):
+    line_list = []
+    line = load_line(line_number).split(" ")
+    for char in line:
+        char = int(char)
+        line_list.append(char)
+    return line_list
