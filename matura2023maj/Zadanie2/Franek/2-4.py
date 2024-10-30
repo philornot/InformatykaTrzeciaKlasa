380efaae0f3e7ed4a0f6950bb421850bdee9cf75
franciszek.alaba <owcarasowa@gmail.com>
2024-10-30 09:30:18 +0100

    zadanie 2 4

---
+++ matura2023maj/Zadanie2/Franek/2-4.py
@@ -0,0 +1,5 @@
+from main import XOR
+a, b = input().split(" ")
+a, b = int(a), int(b)
+print(XOR(a, b))
+XOR(XOR(123,int("101101",1)),int("2D",16))
--- matura2023maj/Zadanie2/Franek/main.py
+++ matura2023maj/Zadanie2/Franek/main.py
@@ -11,4 +11,21 @@ def lista_danych():
     L = list(open("../../dane2023maj/bin.txt", "r"))
     for i in range(len(L)):
         L[i] = L[i].strip()
-    return L
+    return (L)
+
+def XOR(a,b):
+    a1 = bin(a)[2::]
+    b1 = bin(b)[2::]
+    if len(a1) > len(b1):
+        for i in range(len(a1)-len(b1)):
+            b1 = "".join(("0",b1))
+    if len(b1) > len(a1):
+        for i in range(len(b1)-len(a1)):
+            a1 = "".join(("0",a1))
+    wyn = ''
+    for i in range(len(a1)):
+        if a1[i] == b1[i]:
+            wyn += "0"
+        else:
+            wyn += "1"
+    return(int(wyn,2))
