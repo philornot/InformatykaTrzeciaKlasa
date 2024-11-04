513db93365d9c4cae25f88506882f06d8bd83a81
filip.pocztarski <filip.pocztarski@gmail.com>
2024-11-04 09:34:12 +0100

    Zadanie 2.1

---
+++ matura2022maj/Zadanie2/Filip/interfejs.py
@@ -0,0 +1,31 @@
+def algorytm(s):
+    n = len(s)
+
+    A = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        if s[i - 1] == 'a':
+            A[i] = A[i - 1] + 1
+        else:
+            A[i] = A[i - 1]
+
+    B = [0] * (n + 2)
+
+    for j in range(n, 0, -1):
+        if s[j - 1] == 'b':
+            B[j] = B[j + 1] + 1
+        else:
+            B[j] = B[j + 1]
+
+    k = 1
+
+    for i in range(n + 1):
+        if A[i] + B[i + 1] > k:
+            k = A[i] + B[i + 1]
+
+    return k
+
+
+def wynik_algorytmu(s):
+    k = algorytm(s)
+    print(f'{s} --> {k}')
