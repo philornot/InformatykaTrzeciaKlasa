aa24a35da1b304ccd8f7a383961b3c1e58465d97
filip.pocztarski <filip.pocztarski@gmail.com>
2024-11-13 09:07:47 +0100

    4.1

--- matura2022maj/Zadanie4/Filip/41.py
+++ matura2022maj/Zadanie4/Filip/41.py
@@ -1,6 +1,15 @@
 from interfejs import czytaj_plik, przyklad_txt
 
-liczby = czytaj_plik(plik=przyklad_txt)
+# liczby = czytaj_plik(plik=przyklad_txt)
+liczby = czytaj_plik()
+licznik = 0
+pierwsza = None
+
 for liczba in liczby:
     liczba = str(liczba)
-    print(liczba[0])
+    if liczba[0] == liczba[-1]:
+        if licznik == 0:
+            pierwsza = liczba
+        licznik += 1
+
+print(f'Licznik: {licznik}\nPierwsza liczba: {pierwsza}')
