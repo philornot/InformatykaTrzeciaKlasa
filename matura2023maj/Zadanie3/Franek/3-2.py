3ce6bf3d8523c3dd532b37672a30d88e9be37b5f
franciszek.alaba <owcarasowa@gmail.com>
2024-11-14 14:26:25 +0100

    zadanie 3

--- matura2023maj/Zadanie3/Franek/3-2.py
+++ matura2023maj/Zadanie3/Franek/3-2.py
@@ -8,4 +8,20 @@ def pary():
             lpar.append(opc[i]+opc[j])
     return lpar
 
-print(pary(),len(pary()))
+from main import tab, dublet
+
+lds = dublet(tab())
+lp = pary()
+lip = []
+for i in range(100):
+    lip.append(lds.count(lp[i]))
+
+najw = [lp[lip.index(max(lip))],max(lip)]
+print(najw)
+
+najm = ['ph',najw[1]]
+for i in range(100):
+    if lip[i] > 0 and lip[i] < najm[1]:
+        najm[0],najm[1] = lp[lip.index(lip[i])], lip[i]
+
+print(najm)
--- matura2023maj/Zadanie3/Franek/main.py
+++ matura2023maj/Zadanie3/Franek/main.py
@@ -9,3 +9,14 @@ def dublet(L):
     for i in range(len(L)-1):
         lwyn.append(str(L[i])+str(L[i+1]))
     return lwyn
+
+def czyrm(L):
+    cr = [L[0]]
+    i = 1
+    while L[i] > L[i-1]:
+        cr.append(L[i])
+        i += 1
+    cm = L[i::]
+    return cr, cm
+
+print(czyrm([2, 5, 7, 9, 8, 3, 1]))
