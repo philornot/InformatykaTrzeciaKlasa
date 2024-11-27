77582599aeba58f10be99a30deef1265e92dd004
franciszek.alaba <owcarasowa@gmail.com>
2024-11-27 09:18:55 +0100

    zadanie 3/24c

--- matura2024czerwiec/Zadanie3/Franek/3-2.py
+++ matura2024czerwiec/Zadanie3/Franek/3-2.py
@@ -1,2 +1,19 @@
-from main import lista
+from main import lista, ROT13
 l = lista()
+wyn = 0
+lwyn = []
+
+for i in range(len(l)):
+    if ROT13(l[i]) == l[i][::-1]:
+        wyn += 1
+        lwyn.append(l[i])
+
+print(wyn)
+lmax = len(lwyn[0])
+slmax = l[0]
+for i in range(1,wyn):
+    if len(lwyn[i]) > lmax:
+        lmax = len(lwyn[i])
+        slmax = lwyn[i]
+
+print(slmax)
