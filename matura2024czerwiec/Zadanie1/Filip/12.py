512c28861f814161dbe8b4b7cc41211d500d59a4
filip.pocztarski <filip.pocztarski@gmail.com>
2024-11-27 09:24:32 +0100

    nie, nie tak

--- matura2024czerwiec/Zadanie1/Filip/12.py
+++ matura2024czerwiec/Zadanie1/Filip/12.py
@@ -3,16 +3,14 @@ from interfejs import zamien_na_bin
 
 def zapisz_w_tablicy(n, w=3, k=3):
     n = zamien_na_bin(int(n), zwroc_str=True)
-    kratka = {} # todo: idziemy kolumnami, więc trzeba zamienić iterację (kolejność)
+    kratka = {}
     for cyfra in n:
-        print(f'Cyfra {cyfra}')
-        for k_i in range(k):
-            print(f'Kolumna {k_i}')
-            for w_i in range(w):
-                print(f'Wiersz {w_i}')
+        print(f'\nCyfra {cyfra}')
+        for w_i in range(w):
+            for k_i in range(k):
                 kratka[w_i, k_i] = cyfra
-                print(f'{w_i} {k_i} ==> {cyfra}')
+                print(f'({w_i}, {k_i}) = {cyfra}')
 
-
+liczba = 4
+print(f'{liczba} to {zamien_na_bin(liczba)}')
 print(zapisz_w_tablicy(4))
-print(zamien_na_bin(4))
