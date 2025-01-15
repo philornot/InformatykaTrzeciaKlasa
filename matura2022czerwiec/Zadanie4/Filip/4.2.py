e4863b7e6a8f434a5ec59f706a8c63d62e94ca8b
philornot <philornot@users.noreply.github.com>
2025-01-15 09:22:58 +0100

    4.2 całe

--- matura2022czerwiec/Zadanie4/Filip/4.2.py
+++ matura2022czerwiec/Zadanie4/Filip/4.2.py
@@ -1,3 +1,11 @@
-from interfejs import czytaj_liczby_z_pliku, czy_podzielna_przez_17, odbij_liczbe
+from interfejs import czytaj_liczby_z_pliku, odbij_liczbe
 
-liczby = czytaj_liczby_z_pliku(czy_przyklad=True)
+liczby = czytaj_liczby_z_pliku(czy_przyklad=False)
+maks_abs_roznicy = 0, 0
+for liczba in liczby:
+    abs_roznicy = abs(liczba - odbij_liczbe(liczba))
+    if abs_roznicy > maks_abs_roznicy[0]:
+        maks_abs_roznicy= liczba, abs_roznicy
+
+
+print(maks_abs_roznicy)
