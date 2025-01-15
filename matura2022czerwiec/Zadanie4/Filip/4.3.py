1769441c58e3c6f45f81e56e8025ddcc2a2ad92b
philornot <philornot@users.noreply.github.com>
2025-01-15 09:22:46 +0100

    Zmień czy_przyklad na False

--- matura2022czerwiec/Zadanie4/Filip/4.3.py
+++ matura2022czerwiec/Zadanie4/Filip/4.3.py
@@ -1,7 +1,7 @@
 from interfejs import czy_pierwsza, czytaj_liczby_z_pliku, odbij_liczbe
 
 odpowiedz = []
-liczby = czytaj_liczby_z_pliku(czy_przyklad=True)
+liczby = czytaj_liczby_z_pliku(czy_przyklad=False)
 for liczba in liczby:
     if czy_pierwsza(liczba) and czy_pierwsza(odbij_liczbe(liczba)):
         odpowiedz.append(liczba)  # print(liczba, odbij_liczbe(liczba))
