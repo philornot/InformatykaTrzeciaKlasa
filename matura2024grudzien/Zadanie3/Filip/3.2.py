20b47cb6e92d124351eb108a026cb58d4cb9daf4
philornot <philornot@users.noreply.github.com>
2025-01-22 09:06:52 +0100

    Działające 3.2, ale ciut nieoptymalne

--- matura2024grudzien/Zadanie3/Filip/3.2.py
+++ matura2024grudzien/Zadanie3/Filip/3.2.py
@@ -1,12 +1,19 @@
 from interfejs import czy_pierwsza, czytaj_plik
 
-liczby = czytaj_plik(czy_przyklad=True)
-licznik = 0
+liczby = czytaj_plik(czy_przyklad=False)
+licznik = {}
 for liczba in liczby:
-    for i in range(3, liczba):
+    licznik[liczba] = 0
+    for i in range(2, liczba):
         if czy_pierwsza(i):
             if liczba % i == 0:
-                licznik += 1
-                print(liczba, i)
+                licznik[liczba] += 1
 
-print(licznik)
+licznik_liczby = 0
+liczby_odp = []
+for liczba in licznik:
+    if licznik[liczba] == 5:
+        licznik_liczby += 1
+        liczby_odp.append(liczba)
+
+print(liczby_odp, len(liczby_odp))
