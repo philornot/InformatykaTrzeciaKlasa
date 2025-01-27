45c99ce7f8b744d1e0b56590328dbbe2abd75270
philornot <philornot@users.noreply.github.com>
2025-01-27 09:19:20 +0100

    jakies komentarze

--- Algorytmy/rozdzielanie_liczby_na_liste_matematycznie.py
+++ Algorytmy/rozdzielanie_liczby_na_liste_matematycznie.py
@@ -5,9 +5,11 @@ while liczba >  0:
     lista.append(modulo)
     liczba = liczba // 10
 
+# żeby odwrócić listę można użyć [::-1]
 lista_rev = lista[::-1]
 print(lista_rev)
 
+# bez odwracania (działamy na nie nieodwróconej liście)
 n = len(lista)
 liczba = 0
 for i in range(n):
