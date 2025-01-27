767c7ea3104ce6ea0e70fae5fc1fb68473e1436c
philornot <philornot@users.noreply.github.com>
2025-01-27 09:33:25 +0100

    todo :)

--- matura2024grudzien/Zadanie4/Filip/4.2.py
+++ matura2024grudzien/Zadanie4/Filip/4.2.py
@@ -1,6 +1,6 @@
 from interfejs import odczytaj_wymiary
 
-
+# todo zamień kolejnosc arg pls :)
 def czy_prostokat_sie_miesci(szerokosc1, wysokosc1, szerokosc2, wysokosc2):
     if szerokosc1 <= szerokosc2 and wysokosc1 <= wysokosc2:
         return True
@@ -9,4 +9,4 @@ def czy_prostokat_sie_miesci(szerokosc1, wysokosc1, szerokosc2, wysokosc2):
 
 wysokosc_tabela, szerokosc_tabela = odczytaj_wymiary(czy_przyklad=False)
 for i in wysokosc_tabela, szerokosc_tabela:
-    if
+    if czy_prostokat_sie_miesci()
