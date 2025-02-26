7f7d5441e896a59a20d066373f29b5d29531a3f9
franciszek.alaba <owcarasowa@gmail.com>
2025-02-26 09:20:27 +0100

    poddaje sie

--- matura2019maj/Zadanie4/Franek/4-3.py
+++ matura2019maj/Zadanie4/Franek/4-3.py
@@ -1,7 +1,7 @@
 from main import dane, nwd_listy
 
 l = dane()
-pl, dl, nwdc = 0, 1, 0
+#l = [3, 7, 4, 6, 8, 10, 2, 5]
 
 i, tm = 0, []
 odp = []
@@ -10,10 +10,11 @@ while i < len(l):
     if nwd_listy(tm) > 1:
         if len(tm) > len(odp):
             odp = tm
-            print("+")
+            #print("+")
     else:
         tm = [int(l[i])]
-        print(i)
+        #print(i)
     i += 1
+
 
 print(len(odp), odp[0], nwd_listy(odp))
