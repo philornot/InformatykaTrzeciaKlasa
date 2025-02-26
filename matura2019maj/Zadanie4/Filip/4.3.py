78a34b63d8d1005031430293c22afaa2391e4ced
philornot <philornot@users.noreply.github.com>
2025-02-26 09:16:08 +0100

    4.3 !!

--- matura2019maj/Zadanie4/Filip/4.3.py
+++ matura2019maj/Zadanie4/Filip/4.3.py
@@ -1,5 +1,9 @@
 from interfejs import czytaj_plik
 
+liczby = czytaj_plik(czy_przyklad=False)
+# liczby = [3,7,4,6,10,2,5]
+# liczby = [5,70,28,42,98]
+
 def nwd(a, b):
     while b != 0:
         temp = b
@@ -7,9 +11,39 @@ def nwd(a, b):
         a = temp
     return a
 
-liczby = czytaj_plik(czy_przyklad=True)
 
-def nwd_dla_n_liczb(*n):
-    nwd(n, n[:-1])
+def nwd_dla_n_liczb(*argumenty):
+    if len(argumenty) == 1:
+        return argumenty[0]
+    wynik = argumenty[0]
+    for i in range(1, len(argumenty)):
+        wynik = nwd(wynik, argumenty[i])
+    return wynik
 
-nwd_dla_n_liczb(70, 28, 42, 98)
+
+
+maksymalna_dlugosc = 0
+pierwsza_liczba_ciagu = 0
+maksymalny_nwd = 0
+
+for indeks_i in range(len(liczby)):
+    aktualny_nwd = liczby[indeks_i]
+
+    for indeks_j in range(indeks_i + 1, len(liczby)):
+        aktualny_nwd = nwd(aktualny_nwd, liczby[indeks_j])
+
+        if aktualny_nwd > 1:
+            aktualna_dlugosc = indeks_j - indeks_i + 1
+            if aktualna_dlugosc > maksymalna_dlugosc:
+                maksymalna_dlugosc = aktualna_dlugosc
+                pierwsza_liczba_ciagu = indeks_i
+                maksymalny_nwd = aktualny_nwd
+        else:
+            break
+
+pierwsza_liczba = liczby[pierwsza_liczba_ciagu]
+dlugosc_ciagu = maksymalna_dlugosc
+
+print(f"Pierwsza liczba ciągu: {pierwsza_liczba}")
+print(f"Długość ciągu: {dlugosc_ciagu}")
+print(f"Największy wspólny dzielnik: {maksymalny_nwd}")
