6df10348bee056964122068c6b7659c08344c1ab
philornot <philornot@users.noreply.github.com>
2025-03-12 09:30:09 +0100

    interfejs.py do zadania

--- matura2021maj/Zadanie4/Filip/interfejs.py
+++ matura2021maj/Zadanie4/Filip/interfejs.py
@@ -17,11 +17,34 @@ def czytaj_instrukcje(czy_przyklad=False):
     return instrukcje
 
 
+def znajdz_indeks(litera, slowo):
+    indeks = 0
+
+    for i in slowo:
+        if i == litera:
+            return indeks
+        indeks += 1
+
+
 
 def dopisz(litera, obecne_slowo=''):
     return obecne_slowo + litera
 
-def usun(ile_liter, obecne_slowo):
+
+def usun(obecne_slowo, ile_liter=1):
     return obecne_slowo[:-ile_liter]
 
-print(usun(2, '12345'))
+
+def zmien(obecne_slowo, litera):
+    return obecne_slowo.replace(obecne_slowo[-1], litera)
+
+
+def przesun(obecne_slowo, litera):
+    indeks = znajdz_indeks(litera, obecne_slowo)
+    if indeks == -1:
+        return obecne_slowo
+
+    nowa_litera = chr((ord(litera) - ord('A') + 1) % 26 + ord('A')) if litera.isupper() else chr(
+        (ord(litera) - ord('a') + 1) % 26 + ord('a'))
+
+    return obecne_slowo[:indeks] + nowa_litera + obecne_slowo[indeks + 1:]
