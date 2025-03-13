f9855b7c7d0ddf38498d96dfb7d3ceae2e21f23f
philornot <philornot@users.noreply.github.com>
2025-03-13 14:34:06 +0100

    :/

--- matura2016czerwiec/Zadanie4/Filip/4.1.py
+++ matura2016czerwiec/Zadanie4/Filip/4.1.py
@@ -8,8 +8,8 @@ def czy_nalezy_do_okregu(punkty, promien=200, wspolrzedne_srodka=(200, 200)):
     a, b = wspolrzedne_srodka
 
     if (x - a) ** 2 + (y - b) ** 2 > promien ** 2:
-        # print(f'\nDla x={x}, y={y}, a={a}, b={b}, r={promien}, wyrażenie\n'
-         #     f'{(x - a) ** 2 + (y - b) ** 2} jest większe od {promien ** 2}')
+        print(f'\nDla ({x}:{y}) wyrażenie\n'
+             f'{(x - a) ** 2 + (y - b) ** 2} jest większe od {promien ** 2}')
         return False
 
     else:
@@ -20,5 +20,5 @@ for i in lista_z_tuplami_punktow:
     if czy_nalezy_do_okregu(i):
         licznik += 1
 
-# print(f'\n{licznik}')
+print(f'\n{licznik}')
 # print(lista_z_tuplami_punktow)
