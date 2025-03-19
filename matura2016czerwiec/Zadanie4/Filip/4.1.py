e9275e41e1e39503538a98371189113bea8f42eb
philornot <philornot@users.noreply.github.com>
2025-03-19 08:59:08 +0100

    :)

--- matura2016czerwiec/Zadanie4/Filip/4.1.py
+++ matura2016czerwiec/Zadanie4/Filip/4.1.py
@@ -3,22 +3,36 @@ from interfejs import czytaj_tablice_punktow
 lista_z_tuplami_punktow = czytaj_tablice_punktow()
 
 
-def czy_nalezy_do_okregu(punkty, promien=200, wspolrzedne_srodka=(200, 200)):
+def czy_nalezy_do_brzegu_okregu(punkty, promien=200, wspolrzedne_srodka=(200, 200)):
     x, y = punkty
     a, b = wspolrzedne_srodka
+    odleglosc_kw = (x - a) ** 2 + (y - b) ** 2
 
-    if (x - a) ** 2 + (y - b) ** 2 > promien ** 2:
-        print(f'\nDla ({x}:{y}) wyrażenie\n'
-             f'{(x - a) ** 2 + (y - b) ** 2} jest większe od {promien ** 2}')
-        return False
+    # Sprawdzamy czy punkt jest na brzegu
+    return abs(odleglosc_kw - promien ** 2) <= 1
 
-    else:
-        return True
 
-licznik = 0
-for i in lista_z_tuplami_punktow:
-    if czy_nalezy_do_okregu(i):
-        licznik += 1
+def czy_nalezy_do_wnetrza_okregu(punkty, promien=200, wspolrzedne_srodka=(200, 200)):
+    x, y = punkty
+    a, b = wspolrzedne_srodka
+    odleglosc_kw = (x - a) ** 2 + (y - b) ** 2
 
-print(f'\n{licznik}')
-# print(lista_z_tuplami_punktow)
+    # Punkt należy do wnętrza gdy odległość jest mniejsza od promienia
+    return odleglosc_kw < promien ** 2
+
+
+punkty_brzeg = []
+punkty_wnetrze = []
+
+for punkt in lista_z_tuplami_punktow:
+    if czy_nalezy_do_brzegu_okregu(punkt):
+        punkty_brzeg.append(punkt)
+    elif czy_nalezy_do_wnetrza_okregu(punkt):
+        punkty_wnetrze.append(punkt)
+
+# Wypisujemy punkty na brzegu
+for punkt in punkty_brzeg:
+    print(f"{punkt[0]} {punkt[1]}")
+
+# Wypisujemy liczbę punktów we wnętrzu
+print(f"\n{len(punkty_wnetrze)}")
