930ba940c7bdf1f2e493dcd13082d58bcca0d436
franciszek.alaba <owcarasowa@gmail.com>
2025-04-02 09:19:44 +0200

    głupi kod dla zuzdy który działa dla dowolnej liczby

--- Kartkówka/zadanie.py
+++ Kartkówka/zadanie.py
@@ -1,8 +1,8 @@
 def rozbij():
-    liczba = input("Podaj liczbę: ")
-    n, a, liczba = len(liczba), 10 ** (len(liczba) - 1), int(liczba)
+    x = input("Podaj liczbę: ")
+    n, a, x = len(x), 10 ** (len(x) - 1), int(x)
     for i in range(n):
-        print(int(liczba // a))
-        liczba, a  = liczba % a, a/10
+        print(int(x // a))
+        x, a  = x % a, a/10
 
 rozbij()
