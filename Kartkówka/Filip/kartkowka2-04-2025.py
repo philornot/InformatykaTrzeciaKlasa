d52e015b6410d18235f30583d94b264b868f5ac7
philornot <philornot@users.noreply.github.com>
2025-04-02 09:31:10 +0200

    krótsza wersja

--- Kartkówka/Filip/kartkowka2-04-2025.py
+++ Kartkówka/Filip/kartkowka2-04-2025.py
@@ -8,3 +8,11 @@ while liczba > 0:
 print(f'\nCyfry: {lista_cyfr[::-1]}')
 for cyfra in lista_cyfr[::-1]:
     print(cyfra)
+
+# lub krócej:
+# liczba = int(input("Wprowadź liczbę: "))
+# lista_cyfr2 = []
+# while liczba > 0:
+#     lista_cyfr2.append( liczba % 10)
+#     liczba = liczba // 10
+# print(lista_cyfr2[::-1])
