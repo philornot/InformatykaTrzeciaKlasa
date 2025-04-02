055ff8221b265eb0fb6f4349d409a226e222db6e
franciszek.alaba <owcarasowa@gmail.com>
2025-04-02 09:30:24 +0200

    l

--- Kartkówka/zadanie.py
+++ Kartkówka/zadanie.py
@@ -2,7 +2,7 @@ def rozbij():
     x = input("Podaj liczbę: ")
     n, a, x = len(x), 10 ** (len(x) - 1), int(x)
     for i in range(n):
-        print(int(x // a))
+        print(int(x // a), end = "   ")
         x, a  = x % a, a/10
 
 rozbij()
