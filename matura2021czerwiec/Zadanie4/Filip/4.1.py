64a3cc75ed4a0c5a9d1f9075c1b262917a7531d9
philornot <philornot@users.noreply.github.com>
2025-04-28 09:02:59 +0200

    formatowanie i zmiana flagi `czy_przyklad` na False

--- matura2021czerwiec/Zadanie4/Filip/4.1.py
+++ matura2021czerwiec/Zadanie4/Filip/4.1.py
@@ -1,11 +1,11 @@
 from interfejs import czytaj_dane
 
-napisy = czytaj_dane(czy_przyklad=True)
+napisy = czytaj_dane(czy_przyklad=False)
 
 licznik = 0
 for napis in napisy:
     for znak in napis:
-        if znak in ['0','1', '2', '3', '4', '5', '6','7', '8', '9']:
+        if znak in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             licznik += 1
 
 print(licznik)
