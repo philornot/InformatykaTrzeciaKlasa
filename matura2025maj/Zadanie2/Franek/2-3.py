43f2b97e63acc86d6c88da1c03b4828a0cf652a7
franciszek.alaba <owcarasowa@gmail.com>
2025-06-02 09:26:18 +0200

    zadanie 2.3 początek

--- matura2025maj/Zadanie2/Franek/2-3.py
+++ matura2025maj/Zadanie2/Franek/2-3.py
@@ -2,11 +2,18 @@ l = list(open("../../dane2025maj/symbole.txt", "r"))
 for i in range(len(l)):
     l[i] = l[i].strip()
 
+l2 = []
 for i in range(len(l)):
-    for j in range(len(l[i])):
-        if l[i][j] == "o":
-            l[i][j] = "0"
-        if l[i][j] == "+":
-            l[i][j] = "1"
-        if l[i][j] == "*":
-            l[i][j] = "2"
+    a = list(l[i])
+    for j in range(len(a)):
+        if a[j] == "o":
+            a[j] = "0"
+        if a[j] == "+":
+            a[j] = "1"
+        if a[j] == "*":
+            a[j] = "2"
+    l2.append("")
+    for x in a:
+        l2[i] += x
+
+print(l2)
